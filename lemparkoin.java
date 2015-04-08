8cf64e3450d5721859d50f2303fb76b04a87c2ca
mahasaputra <komang.maha@yahoo.com>
2015-04-09 02:25:43 +1200

    Create lemparkoin.java

---
+++ lemparkoin.java
@@ -0,0 +1,17 @@
+
+public class lemparkoin {
+	public static void main ( String[]args) {
+	int n =10;
+	 int i = 1;
+	int bilrandom = (int) (Math.random()+0.5);
+	for (i=1; i<n; i++) {
+	if ( bilrandom==1) {
+		System.out.println("GAMBAR");
+	}
+	else {
+		System.out.println("ANGKA");
+	}
+	}
+	
+	}
+}
